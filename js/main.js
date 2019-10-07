37e2b4d63d9c40fdd28f8b4f0c24652b5a095d92
Richard <madureira.rm@outlook.com>
2019-10-07 00:38:43 -0300

    Basico pt 1

---
+++ js/main.js
@@ -0,0 +1,49 @@
+
+var d = new Date();
+alert(d.getDay());
+
+// var count;
+// for(count = 0; count <= 5; count++){
+//     alert(count);
+// };
+
+// var count = 0;
+// while (count <= 5){
+//     console.log(count);
+//     alert(count);
+//     count++;
+// };
+
+
+// var idade = prompt("Qual sua idade");
+// if(idade >= 18){
+//     alert("maior de idade");        
+// }else{
+//     alert("menor de idade")
+// };
+
+
+// var frutas = [{ nome: "maça", cor: "Vermelha" },{ nome: "uva", cor: "roxa" }]
+// console.log(frutas);
+// alert(frutas[1].nome);
+
+// var fruta = { nome: "maça", cor: "Vermelha" }
+// console.log(fruta.nome);
+// alert(fruta.cor);
+
+
+// var lista = ["maça","pêra","laranja"];
+// lista.push("uva");
+// //lista.pop();
+
+// console.log(lista);
+// console.log(lista.toString());
+// console.log(lista.join(" - "));
+
+
+// var nome  = "Richard martins de madureira";
+// var idade = 23;
+// var idade2 = 10;
+//alert("Bem vindo " + nome + " " + idade);
+// console.log(nome);
+// console.log(idade + idade2);
